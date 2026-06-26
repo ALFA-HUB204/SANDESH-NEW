1c02d2caf25cec90208d56a3308b9d711aca89df
ALFA-HUB204 <blackmovie331@gmail.com>
2026-06-26 13:21:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT_YOUR_SESSON-ID_HERE",
+SESSION_ID: process.env.SESSION_ID || "MZ1D3LBL#s23zmqzROjMPj79gOWVNopm5wDVMKEF-6T4go3TI2vo",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://upld.zone.id/uploads/d4i0x5iq/sandes-md-v2.webp",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 };
